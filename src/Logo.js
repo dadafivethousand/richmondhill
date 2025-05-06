9d4ac5d5dec2db91b95795c9b41621a7f857c2fd
maxim <dada@gmail.com>
2025-05-06 09:47:17 -0400

    initial commit

---
+++ src/Logo.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Stylesheets/Logo.css";
+
+export default function Logo() {
+  return (
+    <div className="logo-container">
+  
+      <span className="logo-title">Richmond Hill</span>
+
+      <div className="belt-wrapper">
+      <div className="belt">
+        <div className="belt-main"></div>
+        <div className="red-bar"></div>
+        <div className="bar-tape top"></div>
+        <div className="bar-tape bottom"></div>
+      </div>
+    </div>
+
+      <span className="logo-subtitle">Jiu-Jitsu</span>
+      
+  
+    </div>
+  );
+}
