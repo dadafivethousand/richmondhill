0dbaea1a478a911b6b0df9e42d73e4c03a783c8e
maxim <dada@gmail.com>
2025-05-11 18:58:30 -0400

    hi

--- src/Navbar.js
+++ src/Navbar.js
@@ -85,15 +85,12 @@ export default function Navbar() {
             <ul className="navbar-options">
             <a  href='#Schedule'>  <li onClick={()=>setClick(false)}>Schedule</li></a>
 
-              <a href='#About'>  <li onClick={()=>setClick(false)}>About Us</li></a>
+              <a href='#Coaches'>  <li onClick={()=>setClick(false)}>About Us</li></a>
                      
-              <a href='#Coaches'>  <li onClick={()=>setClick(false)}>Coaches</li></a>
-              <a href='#programs'>  <li onClick={()=>setClick(false)}>Programs</li></a>
-            <a href='#Pricing'>  <li onClick={()=>setClick(false)}>Pricing</li></a>  
-
- 
+        
+     
               <a href='#FAQ'>  <li onClick={()=>setClick(false)}>FAQ</li></a>
-              <a href='/blog'>  <li onClick={()=>setClick(false)}>Blog</li></a>
+        
 
               {/*<a href='https://billing.stripe.com/p/login/7sI3fAgOUdugbvybII'> <li onClick={()=>setClick(false)}>Members</li></a>  */}
               <a href='#Contact'> <li onClick={()=>setClick(false)}>Contact Us</li></a>
