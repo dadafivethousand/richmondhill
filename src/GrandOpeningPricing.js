914baf302c6ffe1a9b9df03793071e87a5f3ef0d
dadafivethousand <maxli5004@gmail.com>
2025-05-19 13:52:52 -0400

    hi

--- src/GrandOpeningPricing.js
+++ src/GrandOpeningPricing.js
@@ -17,21 +17,21 @@ export default function GrandOpeningPricing() {
             <p className="duration">1 Month</p>
             <p className="price">$120</p>
             <p className="access">Cancel Any Time</p>
-            <a href="#" className="pay-btn" target="_blank" rel="noopener noreferrer">Pay Now</a>
+            <a href="#" className="pay-btn" target="_blank" rel="noopener noreferrer">Get Started</a>
           </div>
 
           <div className="price-tier">
             <p className="duration">6 Months</p>
             <p className="price">$660</p>
             <p className="access">Installment Options Available! </p>
-            <a href="#" className="pay-btn" target="_blank" rel="noopener noreferrer">Pay Now</a>
+            <a href="#" className="pay-btn" target="_blank" rel="noopener noreferrer">Get Started</a>
           </div>
 
           <div className="price-tier">
             <p className="duration">1 Year</p>
             <p className="price">$1100</p>
             <p className="access">Installment Options Available!</p>
-            <a href="#" className="pay-btn" target="_blank" rel="noopener noreferrer">Pay Now</a>
+            <a href="#" className="pay-btn" target="_blank" rel="noopener noreferrer">Get Started</a>
           </div>
         </div>
 
@@ -42,21 +42,21 @@ export default function GrandOpeningPricing() {
             <p className="duration">1 Month</p>
             <p className="price">$140</p>
             <p className="access">Cancel Any Time</p>
-            <a href="#" className="pay-btn" target="_blank" rel="noopener noreferrer">Pay Now</a>
+            <a href="#" className="pay-btn" target="_blank" rel="noopener noreferrer">Get Started</a>
           </div>
 
           <div className="price-tier">
             <p className="duration">6 Months</p>
             <p className="price">$800</p>
             <p className="access">Installment Options Available!</p>
-            <a href="#" className="pay-btn" target="_blank" rel="noopener noreferrer">Pay Now</a>
+            <a href="#" className="pay-btn" target="_blank" rel="noopener noreferrer">Get Started</a>
           </div>
 
           <div className="price-tier">
             <p className="duration">1 Year</p>
             <p className="price">$1500</p>
             <p className="access">Installment Options Available!</p>
-            <a href="#" className="pay-btn" target="_blank" rel="noopener noreferrer">Pay Now</a>
+            <a href="#" className="pay-btn" target="_blank" rel="noopener noreferrer">Get Started</a>
           </div>
         </div>
 
