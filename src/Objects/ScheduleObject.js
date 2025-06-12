f891d9a0063b879234c3028b39838f93be19060e
Max <max@Maxs-MacBook-Air.local>
2025-06-11 21:09:31 -0400

    hi

--- src/Objects/ScheduleObject.js
+++ src/Objects/ScheduleObject.js
@@ -1,43 +1,43 @@
 const schedule = {
     Monday: [
-  
-      { name: "MMA", start: 18.00, end: 19.00 },
-            { name: "BJJ - Gi", start: 19.00, end: 20.00 },
+    { name: "BJJ - NoGi", start: 18.00, end: 19.00 },
+      { name: "MMA", start: 19.00, end: 20.00 },
              { name: "Open Mat", start: 20.00, end: 21.00 }
     ],
 
     Tuesday: [
         { name: "Kids", start: 18.00, end: 19.00 },
-             { name: "BJJ No-Gi", start: 19.00, end: 20.00 },
+             { name: "BJJ Gi", start: 19.00, end: 20.00 },
              { name: "Open Mat", start: 20.00, end: 21.00 }
     ],
     Wednesday: [
  
-     
-      { name: "Muay Thai", start: 18.00, end: 19.00 },
-            { name: "BJJ No-Gi", start: 19.00, end: 20.00 },
+               { name: "BJJ NoGi", start: 18.00, end: 19.00 },
+      { name: "Muay Thai", start: 19.00, end: 20.00 },
+  
              { name: "Open Mat", start: 20.00, end: 21.00 }
     ],
     Thursday: [
       
       { name: "Kids", start: 18.00, end: 19.00 },
-            { name: "BJJ-Gi", start: 19.00, end: 20.00 },
+            { name: "BJJ Gi", start: 19.00, end: 20.00 },
              { name: "Open Mat", start: 20.00, end: 21.00 }
     ],
 
     Friday: [
  
        
-      { name: "MMA", start: 17.00, end: 18.00 },
-            { name: "BJJ-Gi", start: 18.00, end: 19.00 },
-
-            { name: "BJJ No-Gi", start: 19.00, end: 20.00 },
+      
+           
+   { name: "MMA", start: 19.00, end: 20.00 },
+            { name: "BJJ NoGi", start: 18.00, end: 19.00 },
+          
              { name: "Open Mat", start: 20.00, end: 21.00 }
    
     ],
     Saturday: [ 
       { name: "Kids", start: 12, end: 13},
-         { name: "BJJ-Gi", start: 13, end: 14},
+         { name: "BJJ Gi", start: 13, end: 14},
            { name: "Muay-Thai", start: 14, end: 15},
     ]
  
